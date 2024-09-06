4a32b69ea92f4220086332cc3e24d5a52be51f63
Yeison Danner carhuapoma Dett <51946409+yeison7654@users.noreply.github.com>
2024-09-06 16:52:16 -0500

    js html y css

---
+++ btn-redes-sociales/main.js
@@ -0,0 +1,6 @@
+let box_load = document.querySelector(".box-load");
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    box_load.classList.toggle("hidden");
+  }, 1000);
+});
